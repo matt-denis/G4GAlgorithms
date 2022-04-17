6b2a504d372eeebf0faa21c152959e944b468185
matt-denis <m.bocchi@hotmail.com>
2022-04-17 15:44:04 +0200

    correct mistake

--- javasrc/sorting/BubbleSort.java
+++ javasrc/sorting/BubbleSort.java
@@ -6,10 +6,16 @@ public class BubbleSort {
         sort(a, 0, a.length - 1);
     }
 
+    /**
+     * Sorts an array in place between a lower and higher index (included).
+     * @param a array
+     * @param lo lower index
+     * @param hi higher index
+     */
     private static void sort(int[] a, int lo, int hi) {
-        for (int i = hi; i > 0; i--) {
+        for (int i = hi; i > lo; i--) {
             boolean swapped = false;
-            for (int j = 0; j < i; j++) {
+            for (int j = lo; j < i; j++) {
                 if (a[j] > a[j + 1]) {
                     swap(a, j, j + 1);
                     swapped = true;
